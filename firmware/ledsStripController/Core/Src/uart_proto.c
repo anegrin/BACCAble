0f45cc81ac18596050a23fa5adc740e052b00fbb
anegrin <alessandro.negrin@gmail.com>
2025-05-02 12:35:37 +0200

    lookup table for crc8

--- firmware/ledsStripController/Core/Src/uart_proto.c
+++ firmware/ledsStripController/Core/Src/uart_proto.c
@@ -1,26 +1,18 @@
-#import "uart_proto.h"
+#include "uart_proto.h"
 
-void fill_buffer(uint8_t *buffer, uint8_t bufferLength, uint8_t *data, uint8_t dataLength) {
+void fill_buffer(uint8_t *buffer, uint8_t bufferLength, const uint8_t *data, uint8_t dataLength) {
     memset(buffer, 0xFF, bufferLength - 1);
     if (dataLength>bufferLength - 1) dataLength = bufferLength - 1;
     memcpy(buffer, data, dataLength);
     buffer[bufferLength - 1] = calculate_crc8(buffer, bufferLength - 1);
 }
 
-uint8_t calculate_crc8(uint8_t *data, uint8_t length) {
+/* impl takes around 200 ns on STM32H7 */
+uint8_t calculate_crc8(const uint8_t *data, uint8_t length) {
     uint8_t crc = 0x00;
-
     for (uint8_t i = 0; i < length; i++) {
-        crc ^= data[i];
-        for (uint8_t bit = 0; bit < 8; bit++) {
-            if (crc & 0x80) { // if the top bit is set
-                crc = (crc << 1) ^ 0x07; //polynomial x^8 + x^2 + x + 1
-            } else {
-                crc <<= 1;
-            }
-        }
+        crc = CRC8_TABLE[crc ^ data[i]];
     }
-
     return crc;
 }
 
