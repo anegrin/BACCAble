1ef421342c6bac733f7478230e29ddd10cf092ca
anegrin <alessandro.negrin@gmail.com>
2025-05-04 11:44:23 +0200

    first import after changing branch

--- firmware/ledsStripController/Core/Inc/printf_config.h
+++ firmware/ledsStripController/Core/Inc/printf_config.h
@@ -1,7 +1,7 @@
 /*
  * printf_config.h
  *
- *  Created on: May 14, 2025
+ *  Created on: Apr 5, 2025
  *      Author: alessandro
  */
 
@@ -10,7 +10,9 @@
 
  #define PRINTF_DISABLE_SUPPORT_EXPONENTIAL
  #define PRINTF_DEFAULT_FLOAT_PRECISION 3
+ #define PRINTF_MAX_FLOAT 1e6
  #define PRINTF_DISABLE_SUPPORT_LONG_LONG
  #define PRINTF_DISABLE_SUPPORT_PTRDIFF_T
 
  #endif /* INC_PRINTF_CONFIG_H_ */
+ 
